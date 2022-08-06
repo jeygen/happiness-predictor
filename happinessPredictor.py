e87f2f199be2d1bf0d597caabae38efef174246c
jeygen <83786292+jeygen@users.noreply.github.com>
2022-08-06 13:40:27 -0400

    update hp.py

--- happinessPredictor.py
+++ happinessPredictor.py
@@ -1,6 +1,9 @@
+#from locale import getpreferredencoding
 from weather import getTemp
 from crypto import getPrice
 from sentiment import getSentiment
 
 print(getSentiment("bob is very bad."))
+print('Price: ' + str(getPrice()))
+print('Temp: ' + str(getTemp()) )
 
