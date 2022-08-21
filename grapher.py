cf2856d29f512bf22a9a55977e97603a38ef628c
jeygen <83786292+jeygen@users.noreply.github.com>
2022-08-21 17:40:37 -0400

    Grapher now works with front end

--- grapher.py
+++ grapher.py
@@ -2,19 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-location = 'appData.csv'
-df = pd.read_csv(location)
+def graphTail():
 
-x = np.array(df['Time'].tail(5))
-y = np.array(df['Happiness Score'].tail(5))
-# # Define X and Y variable data
-# x = np.array([1, 2, 3, 4])
-# y = x*2
-plt.ylim(-1,1)
+    location = 'appData.csv'
+    df = pd.read_csv(location)
+
+    x = np.array(df['Time'].tail(5))
+    y = np.array(df['Happiness Score'].tail(5))
+    # # Define X and Y variable data
+    # x = np.array([1, 2, 3, 4])
+    # y = x*2
+    plt.ylim(-1,1)
 
 
-plt.plot(x, y, color = 'green')
-plt.xlabel("Time") # add X-axis label
-plt.ylabel("Happiness Score") # add Y-axis label
-plt.title("Happiness vs Time of Day") # add title
-plt.show()
+    plt.plot(x, y, color = 'green')
+    plt.xlabel("Time") # add X-axis label
+    plt.ylabel("Happiness Score") # add Y-axis label
+    plt.title("Happiness vs Time of Day") # add title
+    plt.show()
+
+if __name__ == '__main__':
+    graphTail()
+    print("Graph plotted")
--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter.font import BOLD, ITALIC
 from PIL import ImageTk,Image
 from dataSaver2 import deleteAppData, runSaveAppData
-#from grapher import graphTail
+from grapher import graphTail
 
 root = Tk()
 root.title('Happiness Predictor')
@@ -21,13 +21,13 @@ with open('appWelcome.txt') as f:
     appText = f.read()
 
 my_label = Label(
-				root, image=my_img1, text=appText, compound=CENTER, anchor=CENTER, 
-				font=("Terminal", 24, BOLD), wraplength=500, border=3, relief=RAISED, 
+				root, image=my_img1, text=appText, compound=CENTER, anchor=S, 
+				font=("System", 24, BOLD), wraplength=500, border=3, relief=RAISED, 
 				justify=CENTER, borderwidth=5, padx=10, pady=10, background='green',
 				foreground='black'
 				)
 
-my_label.grid(row=0, column=3, columnspan=3)
+my_label.grid(row=0, column=0, columnspan=5)
 
 '''
 def forward(image_number):
@@ -71,24 +71,24 @@ def getScore():
 def clearData():
 	deleteAppData()
 
-#def graphData():
+def graphData():
 	#pass
-	#graphTail()
+	graphTail()
 
 
 
 #button_back = Button(root, text="<<", command=back, state=DISABLED)
-saveButton = Button(root, text="Save", command=lambda: getScore())
+saveButton = Button(root, text="  Happiness  ", command=lambda: getScore())
 deleteButton = Button(root, text="Delete", command=lambda: deleteAppData())
-#graphButton = Button(root, text="Graph", command=lambda: graphData())
-button_exit = Button(root, text="Exit Program", command=root.quit)
+graphButton = Button(root, text="Graph", command=lambda: graphData())
+button_exit = Button(root, text="Exit", command=root.quit)
 #button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 
 #button_back.grid(row=1, column=0)
-saveButton.grid(row=1, column=1)
-deleteButton.grid(row=1, column=2)
-#graphButton.grid(row=1, column=3)
+saveButton.grid(row=1, column=0)
+graphButton.grid(row=1, column=2)
+deleteButton.grid(row=1, column=3)
 button_exit.grid(row=1, column=4)
 
 #button_forward.grid(row=1, column=2)
